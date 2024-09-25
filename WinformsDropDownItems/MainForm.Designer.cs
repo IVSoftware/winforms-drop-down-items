ee5dae7a7d6a4ef45cc060df18d2722117e687be
Tom Gregor <tgregor@ivsoftware.com>
2024-09-25 06:57:03 -0600

    Add project files.

---
+++ WinformsDropDownItems/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WinformsDropDownItems
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label = new Label();
+            RightClickWindow = new ContextMenuStrip(components);
+            SuspendLayout();
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Font = new Font("Segoe UI", 15F);
+            label.Location = new Point(87, 87);
+            label.Name = "label";
+            label.Size = new Size(302, 41);
+            label.TabIndex = 0;
+            label.Text = "Right-Click Anywhere";
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // RightClickWindow
+            // 
+            RightClickWindow.ImageScalingSize = new Size(24, 24);
+            RightClickWindow.Name = "contextMenuStrip";
+            RightClickWindow.Size = new Size(61, 4);
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 244);
+            Controls.Add(label);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label;
+        private ContextMenuStrip RightClickWindow;
+    }
+}
---
+++ WinformsDropDownItems/MainForm.cs
@@ -0,0 +1,77 @@
+using System.Runtime.ExceptionServices;
+
+namespace WinformsDropDownItems
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            ContextMenuStrip = RightClickWindow;
+
+            var InsertAbove = new ToolStripMenuItem { Text = "InsertAbove", AutoSize = true };
+            var InsertBelow = new ToolStripMenuItem { Text = "InsertBelow", AutoSize = true };
+            foreach (var tsmi in new[] { InsertAbove, InsertBelow })
+            {
+                tsmi.DropDownItems.Add(new ToolStripMenuItem());    // Placeholder
+                tsmi.DropDownOpening += (sender, e) =>
+                {
+                    tsmi.DropDownItems.Clear();
+                    tsmi.DropDownItems.AddRange(CommonItems);
+                };
+                tsmi.DropDownClosed += (sender, e) =>
+                {
+                    tsmi.DropDownItems.Clear();
+                    tsmi.DropDownItems.Add(new ToolStripMenuItem()); // Placeholder
+                };
+                RightClickWindow.Items.Add(tsmi);
+            }
+        }
+        ToolStripItem[] CommonItems
+        {
+            get
+            {
+                if (_commonItemsSingleton is null)
+                {
+                    var clickableItem = new ToolStripMenuItem
+                    {
+                        Text = "Simple click"
+                    };
+                    clickableItem.Click += SimpleClick_Clicked;
+
+                    var comboBoxItem = new ToolStripComboBox
+                    {
+                        DropDownStyle = ComboBoxStyle.DropDownList,
+                    };
+
+                    comboBoxItem.Items.AddRange(new[]
+                    {
+                        "Select",
+                        "Item A",
+                        "Item B",
+                        "Item C",
+                    });
+
+                    comboBoxItem.SelectedIndex = 0;
+                    comboBoxItem.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
+                    _commonItemsSingleton = new ToolStripItem[]
+                    {
+                        clickableItem,
+                        new ToolStripSeparator(),
+                        comboBoxItem,
+                    };
+                }
+                return _commonItemsSingleton;
+            }
+        }
+
+        private void ComboBox_SelectedIndexChanged(object? sender, EventArgs e){ }
+            // MessageBox.Show($"{(sender as ToolStripItem)?.Text}");
+
+        private void SimpleClick_Clicked(object? sender, EventArgs e) =>
+            MessageBox.Show($"{(sender as ToolStripItem)?.Text}");
+
+        ToolStripItem[]? _commonItemsSingleton = default;
+
+    }
+}
